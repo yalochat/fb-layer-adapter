bcfa9f01d7a229eabd13226c05b5bf665bff47d5
Fred Allen <edwin.allen@gmail.com>
2016-07-04 14:24:13 -0600

    [Fix] move configuration to config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var Confidence = require('confidence');
 
 var config = {
-    '$meta': 'FB Messenger api ',
+    '$meta': 'FB Messenger Layer adapter',
     'app': {
         'env':  process.env.APP_ENV || 'local',
         'fb': {
@@ -14,7 +14,25 @@ var config = {
                 test: process.env.APP_FB_URL || 'https://graph.facebook.com/v2.6'
             },
             'token': process.env.APP_FB_TOKEN
+        },
+      'layer':{
+        token: {
+          $filter: 'env',
+          $default: '7R3ESPh1NGHciYiGladYRaBPlxWLTqeS2n8PlszSVR1TMN7F',
+          local: process.env.LAYER_TOKEN || '7R3ESPh1NGHciYiGladYRaBPlxWLTqeS2n8PlszSVR1TMN7F',
+          test: process.env.LAYER_TOKEN || '7R3ESPh1NGHciYiGladYRaBPlxWLTqeS2n8PlszSVR1TMN7F',
+          production: process.env.LAYER_TOKEN,
+          staging: process.env.LAYER_TOKEN
+        },
+        appId: {
+          $filter: 'env',
+          $default: 'bec9d548-6265-11e5-9a48-0edffe00788f',
+          test: process.env.LAYER_APP_ID || 'bec9d548-6265-11e5-9a48-0edffe00788f',
+          local: process.env.LAYER_APP_ID || 'bec9d548-6265-11e5-9a48-0edffe00788f',
+          stagin: process.env.LAYER_APP_ID,
+          production: process.env.LAYER_APP_ID
         }
+      }
     },
     'logger': {
         'options': {
@@ -23,16 +41,16 @@ var config = {
     }
 };
 
-var store = new Confidence.Store(config);
+var store = new Confidence.Store(config)
 
 
 exports.get = function (key, criteria) {
-    if(!criteria) criteria = {};
-    return store.get(key, criteria);
+  if(!criteria) criteria = {env: store.get('/app/env')}
+    return store.get(key, criteria)
 };
 
 
 exports.meta = function (key, criteria) {
-    if(!criteria) criteria = {};
-    return store.meta(key, criteria);
+    if(!criteria) criteria = {}
+    return store.meta(key, criteria)
 };
---
+++ test/config.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Lab = require('lab')
+const Code = require('code')
+const Config = require('../config')
+
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const before = lab.before
+const after = lab.after
+const expect = Code.expect
+
+describe('Config', () => {
+
+  it('gets config data', (done) => {
+
+    expect(Config.get('/')).to.be.an.object()
+
+    done()
+  })
+
+
+  it('it gets config meta data', (done) => {
+
+    expect(Config.meta('/')).to.match(/FB Messenger Layer adapter/i)
+
+    done()
+  })
+
+  it('it gets config layer credentials', (done) => {
+    expect(Config.get('/app/layer/token')).to.equals('7R3ESPh1NGHciYiGladYRaBPlxWLTqeS2n8PlszSVR1TMN7F')
+    expect(Config.get('/app/layer/appId')).to.equals('bec9d548-6265-11e5-9a48-0edffe00788f')
+    done()
+  })
+
+})
