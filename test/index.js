18c728ac3b6c2e5d05189289d8d86e3f3f8f19a5
Yoel Monzon <yoelfme@hotmail.com>
2016-07-22 14:09:37 -0500

    [update] test for index.js

--- test/index.js
+++ test/index.js
@@ -19,23 +19,16 @@ const beforeEach = lab.beforeEach
 describe('Index module', () => {
 
   it('loads messenger plugin', (done) => {
+    const Messenger = new Adapter.Messenger({ token: 'ABC123'})
 
-    new Adapter.Messenger({ token: 'ABC123'})
-      .then(messengerInstance => {
-
-        expect(messengerInstance).to.be.instanceOf(Adapter.Messenger)
-        done()
-      })
-    })
+    expect(Messenger).to.be.instanceOf(Adapter.Messenger)
+    done()
+  })
 
   it('loads layer plugin', (done) => {
+    const Layer = new Adapter.Layer({ token: 'ABC123', appId: '1234', sender: {}, messengerToken: 'CBA321'})
 
-    new Adapter.Layer({ token: 'ABC123', appId: '1234', sender: {}, messengerToken: 'CBA321'})
-      .then(layerInstance => {
-
-        expect(layerInstance).to.be.instanceOf(Adapter.Layer)
-        done()
-      })
-    })
-
+    expect(Layer).to.be.instanceOf(Adapter.Layer)
+    done()
+  })
 })
